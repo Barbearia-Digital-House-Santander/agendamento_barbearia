ebdf28454aa549bef86d2b56978c07977d6492dd
Lorena Rodrigues <lorena.rrochalima@gmail.com>
2020-09-30 22:37:04 -0300

    Criar classe Constante

---
+++ src/main/java/br/dh/barbearia/java/commun/Constantes.java
@@ -0,0 +1,9 @@
+package br.dh.barbearia.java.commun;
+
+public class Constantes {
+		
+		public static int TAMANHO_RANDOM = 10;
+		
+		public static String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; 
+
+}
