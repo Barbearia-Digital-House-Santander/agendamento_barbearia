334c443ced8046c283fb1027a993598a30724195
Lorena Rodrigues <lorena.rrochalima@gmail.com>
2020-10-05 22:44:59 -0300

    alteração Spring application

--- src/main/java/br/dh/barbearia/java/application/AgendamentoBarbeariaDhApplication.java
+++ src/main/java/br/dh/barbearia/java/application/AgendamentoBarbeariaDhApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = { "br.dh.barbearia.java.entity" })
 
 @SpringBootApplication
 public class AgendamentoBarbeariaDhApplication extends SpringBootServletInitializer{
