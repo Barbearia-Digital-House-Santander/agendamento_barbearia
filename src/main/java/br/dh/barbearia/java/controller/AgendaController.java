8fbe64c7a4ec20861c295abb7e51ead6f6dbdb91
Lorena Rodrigues <lorena.rrochalima@gmail.com>
2020-10-11 18:23:25 -0300

    refatoração de códigos

--- src/main/java/br/dh/barbearia/java/controller/AgendaController.java
+++ src/main/java/br/dh/barbearia/java/controller/AgendaController.java
@@ -1,54 +1,33 @@
 package br.dh.barbearia.java.controller;
 
-import java.time.LocalDate;
 import java.util.Date;
-import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import br.dh.barbearia.java.commun.Constantes;
-import br.dh.barbearia.java.commun.DataUtils;
-import br.dh.barbearia.java.entity.Agenda;
 import br.dh.barbearia.java.repository.AgendaRepository;
 import br.dh.barbearia.java.service.AgendaService;
 
 @Controller
 @RequestMapping("/barbearia")
 public class AgendaController {
-	
-	  private AgendaRepository agendaRepository;
 	  
 	  @Resource
 	  private AgendaService agendaService;
 
       @Autowired
       public AgendaController( AgendaRepository agendaRepository) {
-            this.agendaRepository = agendaRepository;
       }
 	  
 	  @RequestMapping(value = "/agendamento", method = RequestMethod.POST)
       public String adicionaAgenda(String cpf, String nome, String servico, Date dataAgendamento, String horaAgendamento,
-    		  String genero, String email, String numProc,String telefone, Agenda agenda) {
-		  agenda.setCpf(cpf);
-		  agenda.setNome(nome);
-		  agenda.setServico(servico);
-		  agenda.setDataAgendamento(DataUtils.DataSemHoras(dataAgendamento));
-		  agenda.setHoraAgendamento(horaAgendamento);
-		  agenda.setGenero(genero);
-		  agenda.setEmail(email);
-		  agenda.setTelefone(telefone);
+    		  String genero, String email, String chaveDeCancelamento,String telefone) {
 		  
-		  agenda.setCancelado(Constantes.NAO);
-		  agenda.setChaveDeCancelamento(agendaService.geradorAleatorio());
-		  
-          agendaRepository.save(agenda);
-          return "redirect:/mensagem/agendamento/ok";
+          return agendaService.salvarMarcacaoNaAgenda( cpf,  nome,  servico,  dataAgendamento,  horaAgendamento, genero,  email,  chaveDeCancelamento, telefone);
       }
 	  
 	  @RequestMapping(value = "/agendamento", method = RequestMethod.GET)
@@ -57,28 +36,8 @@ public class AgendaController {
       }
 	  
 	  @RequestMapping(value = "/cancelamento", method = RequestMethod.POST)
-      public String adicionaAgenda(String chaveDeCancelamento, Agenda agenda) {
-		  List<Agenda> dados = agendaService.buscaDadosClientePelaChaveDeCancelamento(chaveDeCancelamento);
-		  
-		  if(!dados.isEmpty()) {
-			  agenda.setIdAgendamento(dados.get(0).getIdAgendamento());
-			  agenda.setCpf(dados.get(0).getCpf());
-			  agenda.setNome(dados.get(0).getNome());
-			  agenda.setServico(dados.get(0).getServico());
-			  agenda.setDataAgendamento(dados.get(0).getDataAgendamento());
-			  agenda.setHoraAgendamento(dados.get(0).getHoraAgendamento());
-			  agenda.setGenero(dados.get(0).getGenero());
-			  agenda.setEmail(dados.get(0).getEmail());
-			  agenda.setTelefone(dados.get(0).getTelefone());
-			  agenda.setChaveDeCancelamento(chaveDeCancelamento);
-			  agenda.setCancelado(Constantes.SIM);
-			  agenda.setDataCancelamento(LocalDate.now());
-			  agendaRepository.save(agenda);
-	          return "redirect:/mensagem/cancelamento/ok";
-		  }
-		  else {
-          return "redirect:/mensagem/cancelamento/falha";
-          }
+      public String atualizarAgenda(String chaveDeCancelamento) {
+		  return agendaService.atualizarMarcacaoNaAgenda(chaveDeCancelamento);
       }
 	
 	  @RequestMapping(value = "/cancelamento", method = RequestMethod.GET)
--- src/main/java/br/dh/barbearia/java/service/AgendaService.java
+++ src/main/java/br/dh/barbearia/java/service/AgendaService.java
@@ -1,5 +1,7 @@
 package br.dh.barbearia.java.service;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -7,6 +9,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import br.dh.barbearia.java.commun.Constantes;
+import br.dh.barbearia.java.commun.DataUtils;
 import br.dh.barbearia.java.commun.RandomCommun;
 import br.dh.barbearia.java.entity.Agenda;
 import br.dh.barbearia.java.repository.AgendaRepository;
@@ -16,13 +19,59 @@ public class AgendaService {
 
 	@Resource
 	private AgendaRepository agendaRepository;
+
 	
-	public String geradorAleatorio() {
+	private String geradorAleatorio() {
 		RandomCommun random = new RandomCommun();
 		return random.geradorLetrasNumerosAleatorios(Constantes.CARACTERES, Constantes.TAMANHO_RANDOM);
 	}
 
-	public List<Agenda> buscaDadosClientePelaChaveDeCancelamento(String chaveDeCancelamento) {
+	private List<Agenda> buscaDadosClientePelaChaveDeCancelamento(String chaveDeCancelamento) {
 		return agendaRepository.findByChaveDeCancelamento(chaveDeCancelamento);
+	}
+	
+	public String salvarMarcacaoNaAgenda(String cpf, String nome, String servico, Date dataAgendamento, String horaAgendamento,
+  		  String genero, String email, String chaveDeCancelamento,String telefone) {
+
+		  Agenda agenda = new Agenda();
+		  agenda.setCpf(cpf);
+		  agenda.setNome(nome);
+		  agenda.setServico(servico);
+		  agenda.setDataAgendamento(DataUtils.DataSemHoras(dataAgendamento));
+		  agenda.setHoraAgendamento(horaAgendamento);
+		  agenda.setGenero(genero);
+		  agenda.setEmail(email);
+		  agenda.setTelefone(telefone);
+		  
+		  agenda.setCancelado(Constantes.NAO);
+		  agenda.setChaveDeCancelamento(geradorAleatorio());
+		  
+		  agendaRepository.save(agenda);
+		return "redirect:/mensagem/agendamento/ok";
+	}
+	
+	public String atualizarMarcacaoNaAgenda(String chaveDeCancelamento) {
+		List<Agenda> dados = buscaDadosClientePelaChaveDeCancelamento(chaveDeCancelamento);
+	    Agenda agenda = new Agenda();
+
+		  if(!dados.isEmpty()) {
+			  agenda.setIdAgendamento(dados.get(0).getIdAgendamento());
+			  agenda.setCpf(dados.get(0).getCpf());
+			  agenda.setNome(dados.get(0).getNome());
+			  agenda.setServico(dados.get(0).getServico());
+			  agenda.setDataAgendamento(dados.get(0).getDataAgendamento());
+			  agenda.setHoraAgendamento(dados.get(0).getHoraAgendamento());
+			  agenda.setGenero(dados.get(0).getGenero());
+			  agenda.setEmail(dados.get(0).getEmail());
+			  agenda.setTelefone(dados.get(0).getTelefone());
+			  agenda.setChaveDeCancelamento(chaveDeCancelamento);
+			  agenda.setCancelado(Constantes.SIM);
+			  agenda.setDataCancelamento(LocalDate.now());
+			  agendaRepository.save(agenda);
+	          return "redirect:/mensagem/cancelamento/ok";
+		  }
+		  else {
+			  return "redirect:/mensagem/cancelamento/falha";
+        }
 	}
 }
