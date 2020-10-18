5746fca2231324aa6f1a0649f988d7679849fe89
Lorena Rodrigues <lorena.rrochalima@gmail.com>
2020-10-18 19:08:41 -0300

    Criando controllers das páginas comuns/menus

---
+++ src/main/java/br/dh/barbearia/java/controller/TemplateController.java
@@ -0,0 +1,30 @@
+package br.dh.barbearia.java.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/barbearia")
+public class TemplateController {
+	
+	  @Autowired
+	  public TemplateController() {
+		// TODO Auto-generated constructor stub
+	}
+	  @RequestMapping(value = "/galeria", method = RequestMethod.GET)
+      public String abrirPaginaGaleria() {
+            return "galeria";
+      }
+	  
+	  @RequestMapping(value = "/servicos", method = RequestMethod.GET)
+      public String abrirPaginaServicos() {
+            return "servicos";
+      }
+	  
+	  @RequestMapping(value = "/sobre", method = RequestMethod.GET)
+      public String abrirPaginaSobre() {
+            return "sobre";
+      }
+}
