a6f1595f558689f1582e91f793d7e39fd7b1340b
YtaloWill <ytalowillgloria@gmail.com>
2020-11-26 21:28:28 -0300

    add categoria repository padrao

--- src/main/java/br/dh/barbearia/java/repository/CategoriaRepository.java
+++ src/main/java/br/dh/barbearia/java/repository/CategoriaRepository.java
@@ -1,15 +1,11 @@
 package br.dh.barbearia.java.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.dh.barbearia.java.entity.Categoria;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-	List<Categoria> findAll();
-	
 }
